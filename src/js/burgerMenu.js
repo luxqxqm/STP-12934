205febabb18f197225783d83a6e57d52d73acf66
Den Yaseniuk <denasenuk@gmail.com>
2026-06-07 13:00:18 +0200

    features sectin + burgerMenu no-scroll

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -5,9 +5,11 @@ const menu = document.querySelector('[data-visible]');
 openBtn.addEventListener('click', e => {
   e.preventDefault();
   menu.dataset.visible = 'open';
+  document.body.classList.add('no-scroll');
 });
 
 closeBtn.addEventListener('click', e => {
   e.preventDefault();
   menu.dataset.visible = 'close';
+  document.body.classList.remove('no-scroll');
 });
