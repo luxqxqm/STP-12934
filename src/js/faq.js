794e21e107baefe8e6c705b2e245783c19ae30fd
Tetiana Yushkevych <139336268+YshTi@users.noreply.github.com>
2026-06-07 18:55:19 +0200

    Refactor FAQ functionality with state management

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,20 +1,40 @@
-const buttons = document.querySelectorAll('[data-button]');
+const faqItems = document.querySelectorAll('[data-item]');
 
-buttons.forEach(button => {
-  button.addEventListener('click', e => {
-    e.preventDefault();
+const iconPlus = './img/sprite.svg#icon-plus';
+const iconMinus = './img/sprite.svg#icon-minus';
 
-    const item = button.closest('[data-item]');
-    const answer = item.querySelector('[data-answer]');
-    const svg = item.querySelector('[data-svg]');
+const setFaqState = (item, isOpen) => {
+  const button = item.querySelector('[data-button]');
+  const icon = item.querySelector('[data-svg]');
 
-    answer.classList.toggle('active');
-    button.classList.toggle('plus');
+  item.dataset.state = isOpen ? 'open' : 'closed';
 
-    if (svg.href.baseVal.includes('icon-plus')) {
-      svg.href.baseVal = '../img/sprite.svg#icon-minus';
-    } else {
-      svg.href.baseVal = '../img/sprite.svg#icon-plus';
+  if (button) {
+    button.setAttribute('aria-expanded', String(isOpen));
+  }
+
+  if (icon) {
+    icon.setAttribute('href', isOpen ? iconMinus : iconPlus);
+  }
+};
+
+faqItems.forEach((item, index) => {
+  const button = item.querySelector('[data-button]');
+
+  setFaqState(item, index === 0);
+
+  button?.addEventListener('click', event => {
+    event.preventDefault();
+
+    const isOpen = item.dataset.state === 'open';
+
+    faqItems.forEach(faqItem => {
+      setFaqState(faqItem, false);
+    });
+
+    if (!isOpen) {
+      setFaqState(item, true);
     }
   });
 });
+
