6fa46ce42e79c741e526ead056900f46d9c3efb6
YushTi <zebra2368@gmail.com>
2026-06-07 19:10:31 +0200

    modified svg for list and their path. removed path from js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,20 +1,45 @@
-const buttons = document.querySelectorAll('[data-button]');
+const faqItems = document.querySelectorAll('[data-item]');
 
-buttons.forEach(button => {
-  button.addEventListener('click', e => {
-    e.preventDefault();
+const getSpritePath = useElement => {
+  const currentHref = useElement.getAttribute('href');
 
-    const item = button.closest('[data-item]');
-    const answer = item.querySelector('[data-answer]');
-    const svg = item.querySelector('[data-svg]');
+  return currentHref.split('#')[0];
+};
 
-    answer.classList.toggle('active');
-    button.classList.toggle('plus');
+const setFaqState = (item, isOpen) => {
+  const button = item.querySelector('[data-button]');
+  const icon = item.querySelector('[data-svg]');
 
-    if (svg.href.baseVal.includes('icon-plus')) {
-      svg.href.baseVal = '../img/sprite.svg#icon-minus';
-    } else {
-      svg.href.baseVal = '../img/sprite.svg#icon-plus';
+  item.dataset.state = isOpen ? 'open' : 'closed';
+
+  if (button) {
+    button.setAttribute('aria-expanded', String(isOpen));
+  }
+
+  if (icon) {
+    const spritePath = getSpritePath(icon);
+    const iconName = isOpen ? 'icon-minus' : 'icon-plus';
+
+    icon.setAttribute('href', `${spritePath}#${iconName}`);
+  }
+};
+
+faqItems.forEach((item, index) => {
+  const button = item.querySelector('[data-button]');
+
+  setFaqState(item, index === 0);
+
+  button?.addEventListener('click', event => {
+    event.preventDefault();
+
+    const isOpen = item.dataset.state === 'open';
+
+    faqItems.forEach(faqItem => {
+      setFaqState(faqItem, false);
+    });
+
+    if (!isOpen) {
+      setFaqState(item, true);
     }
   });
 });
